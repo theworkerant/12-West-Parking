f51f27b77639c276e04753fde322b1785196d6b4
Graham Powrie <graham@votary.net>
2010-09-29 14:53:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkapp::Application.config.secret_token = '805b890cf1b64413fa6942d7043dd5acccc1d46c8b1eb09b625195b55da4d940e328dd8f12a73974ce262bd81917a8858fba4fd37908db2f8a71a13740c1cfb4'
